0729767eba161deefb17f2a5d41685e786d5dd3f
SashaDance <sksslr@mail.ru>
2023-10-13 13:53:05 +0300

    some changes in GetData class

--- get_data.py
+++ get_data.py
@@ -1,34 +1,35 @@
 import pandas as pd
 import numpy as np
 import pickle
+from demands_split import split_demands
 
 VEHICLE_CAPACITY = 54
 
 
 class GetData:
-    def __init__(self):
-        self.sc_to_sc_distances = pd.read_csv('Data/sc_to_sc_updated.csv')
-        self.ff_to_sc_distances = pd.read_csv('Data/ff_to_sc_updated.csv')
-        self.sc_indices = dict()
+    def __init__(self, ff: int):
+        self.sc_to_sc_distances = pd.read_excel('data/sc_to_sc_distances.xlsx')
+        self.ff_to_sc_distances = pd.read_excel('data/ff_to_sc_distances.xlsx')
+        self.ind_to_sc_map = dict()
+        self.ff = ff
 
-        with open('Data/demands.pickle', 'rb') as f:
+        with open('data/demands.pickle', 'rb') as f:
             self.demands_dict = pickle.load(f)
 
-    def get_distance_matrix(self, ff: int) -> list[list]:
-        sort_centers = self.demands_dict[ff].keys()
+    def get_distance_matrix(self) -> list[list]:
+        sort_centers = self.demands_dict[self.ff].keys()
         # initializing the array with n + 1 rows
         # where n is number of sort centers and 1 is ff
         distance_matrix = np.zeros(
             shape=((len(sort_centers) + 1), (len(sort_centers) + 1))
         ).tolist()
-        '''
+        """
         filling the distances from ff to sort centers 
         (first row and first column)
-        '''
+        """
         for i, sc in enumerate(sort_centers):
-            sc = str(sc)  # type of 'Код СЦ' column is object
             distance_matrix[0][i + 1] = self.ff_to_sc_distances[
-                (self.ff_to_sc_distances['Код ФФ'] == ff)
+                (self.ff_to_sc_distances['Код ФФ'] == self.ff)
                 & (self.ff_to_sc_distances['Код СЦ'] == sc)
             ]['Distance'].values[0]
             distance_matrix[i + 1][0] = distance_matrix[0][i + 1]
@@ -36,8 +37,6 @@ class GetData:
         # filling the distances from sort center to sort center
         for i, sc1 in enumerate(sort_centers, start=1):
             for j, sc2 in enumerate(sort_centers, start=1):
-                sc1 = str(sc1)  # type of 'Код СЦ' column is object
-                sc2 = str(sc2)
                 distance_matrix[i][j] = self.sc_to_sc_distances[
                     (self.sc_to_sc_distances['Код СЦ1'] == sc1)
                     & (self.sc_to_sc_distances['Код СЦ2'] == sc2)
@@ -45,20 +44,31 @@ class GetData:
 
         return distance_matrix
 
-    def get_demands(self, ff: int) -> list:
-        '''
+    def get_demands(self) -> list:
+        """
         initializing the array with zero
         where zero is the demand for ff from ff
-        '''
+        """
 
         demands = [0]
 
-        for ind, item in enumerate(self.demands_dict[ff].items(), start=1):
+        for ind, item in enumerate(self.demands_dict[self.ff].items(), start=1):
             key, demand = item
             demands.append(demand)
-            self.sc_indices[ind] = key
+            self.ind_to_sc_map[ind] = key
 
         return demands
+
+    def get_data(self) -> tuple[list[list], list]:
+
+        distance_matrix = self.get_distance_matrix()
+        demands = self.get_demands()
+
+        distance_matrix, demands = split_demands(self)
+
+        return distance_matrix, demands
+
+
 
     @staticmethod
     def print_matrix(matrix: list) -> None:
